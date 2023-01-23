047d3663d3cdf217789c7ca6713644375ce707b6
Jeff Morrow <jwmorrow1s@gmail.com>
2023-01-22 22:00:59 -0600

    removed dead code

--- src/svdjinni.lisp
+++ src/svdjinni.lisp
@@ -1,5 +1,4 @@
 (defconstant +xml-header-tag+ "<?xml version=\"1.0\" standalone=\"no\"?>")
-(defconstant +svg-header-tag+ "<svg version=\"1.1\" width=\"120\" height=\"100\" xmlns=\"http://www.w3.org/2000/svg\">")
 (defconstant +svg-footer-tag+ "</svg>")
 
 (declaim (ftype (function (list) boolean) string-list-p))
@@ -21,13 +20,6 @@
     :if-exists :overwrite
     :if-does-not-exist :create)
     (dolist (line `(,+xml-header-tag+ ,(svg-header-tag width height) ,@content ,+svg-footer-tag+))
-      (write-line line file))))
-
-(defun do-thing ()
-  (with-open-file (file #p"test.svg" :direction :output
-    :if-exists :overwrite
-    :if-does-not-exist :create)
-    (dolist (line `(,+xml-header-tag+ ,+svg-header-tag+ ,+svg-footer-tag+))
       (write-line line file))))
 
 (defun main (&rest argv)
