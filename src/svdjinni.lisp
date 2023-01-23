b99856f866acfe8416375a46c48a9c54b4a31395
Jeff Morrow <jwmorrow1s@gmail.com>
2023-01-22 21:59:33 -0600

    added with-svg fn

--- src/svdjinni.lisp
+++ src/svdjinni.lisp
@@ -2,13 +2,34 @@
 (defconstant +svg-header-tag+ "<svg version=\"1.1\" width=\"120\" height=\"100\" xmlns=\"http://www.w3.org/2000/svg\">")
 (defconstant +svg-footer-tag+ "</svg>")
 
+(declaim (ftype (function (list) boolean) string-list-p))
+(defun string-list-p (lst)
+  (every #'(lambda (x) (typep x 'string)) lst))
+
+(deftype string-list ()
+  `(and (satisfies consp)
+        (satisfies string-list-p)))
+
+(declaim (ftype (function (fixnum fixnum) string) svg-header-tag))
+(defun svg-header-tag (width height)
+  (format nil "<svg version=\"1.1\" width=\"~D\" height=\"~D\" xmlns=\"http://www.w3.org/2000/svg\">"
+          width height))
+
+(declaim (ftype (function (fixnum fixnum pathname string-list) t) with-svg))
+(defun with-svg (width height destination content)
+  (with-open-file (file destination :direction :output
+    :if-exists :overwrite
+    :if-does-not-exist :create)
+    (dolist (line `(,+xml-header-tag+ ,(svg-header-tag width height) ,@content ,+svg-footer-tag+))
+      (write-line line file))))
+
 (defun do-thing ()
   (with-open-file (file #p"test.svg" :direction :output
     :if-exists :overwrite
     :if-does-not-exist :create)
-    (dolist (line (list +xml-header-tag+ +svg-header-tag+ +svg-footer-tag+))
+    (dolist (line `(,+xml-header-tag+ ,+svg-header-tag+ ,+svg-footer-tag+))
       (write-line line file))))
 
 (defun main (&rest argv)
   (declare (ignorable argv))
-  (do-thing))
+  (with-svg 120 100 #p"test.svg" '("hello" "there")))
